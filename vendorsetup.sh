863d8ec66b41c336aeb5f553a4975e465b64b0c2
yincen17 <52388255+yincen17@users.noreply.github.com>
2021-05-17 23:01:57 +0800

    clone android_vendor_qcom_opensource_cryptfs_hw

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,3 +9,5 @@ git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-
 git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.0-caf-msm8996 hardware/qcom-caf/msm8996/media
 git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.0-caf-msm8996 hardware/qcom-caf/msm8996/audio
 echo "cloned hals"
+git clone https://github.com/LineageOS/android_vendor_qcom_opensource_cryptfs_hw -b lineage-18.1 vendor/qcom/openspurce/cryptfs/hw
+echo "cloned cryptfs_hw"
